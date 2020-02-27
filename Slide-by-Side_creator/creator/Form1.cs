1a1e6a59fa0170cb54c9a669c160d3e26c2ae9a8
F1shOutaWater <jared.boyd0814@gmail.com>
2020-02-27 16:22:33 -0600

    Added Music Buttons and Functionality
    Initial addition of buttons and functionality. @TODO: It will require additional modifications.

--- Slide-by-Side_creator/creator/Form1.cs
+++ Slide-by-Side_creator/creator/Form1.cs
@@ -8,35 +8,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Media;
+using System.Timers;
 
 namespace test
 {
     public partial class Form1 : Form
     {
         FolderBrowserDialog d = new FolderBrowserDialog();      // create the folder broswer dialog 'd'
+        
+        //Variable Declaration
+        //Temp Music Track Handling Variables
         OpenFileDialog ofd = new OpenFileDialog();
+        private readonly string currentTrackName = "No Track Selected";
+        private string currentMusicPathName = "";
+        bool musicPlaying = false;
+        private System.Windows.Forms.Timer time = new System.Windows.Forms.Timer();
+
 
         public Form1()
         {
 
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-
-
         }
 
         private void browseButton_Click(object sender, EventArgs e)
@@ -73,15 +66,78 @@ namespace test
             this.thumbnailLayoutPanel.AutoScroll = true;                            // add the scrollbar to the layout panel
         }
 
-        private void musicButton_Click_1(object sender, EventArgs e)
+        private void Music_Select_Button_Click(object sender, EventArgs e)
         {
             DialogResult drResult = d.ShowDialog();
-            //if ()
+            if (drResult == System.Windows.Forms.DialogResult.OK)           // make sure the path is ok
+            {
+                string[] v = Directory.GetFiles(d.SelectedPath, "*.wav");
+                currentMusicPathName = v[0];  // make a variable for object with .wav file in the selected path
+            }
+
+            //@TODO: Set Track Name
+            //musicNameBox1.Text = currentTrackName;
+            string PathPrinted = currentMusicPathName;
+            musicNameBox1.Text = PathPrinted;
+
+            //@TODO: Set Track Duration
+            progressBar.Value = progressBar.Minimum;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void Music_Test_Click(object sender, EventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = currentMusicPathName;
+            if (musicPlaying == false)
+            {
+                player.Play(); //start music
+                musicPlaying = true; 
+                progressBar.Value = 0; //reset progress bar
+                MusicTimer(); //start progress bar timer
+            }
+            else
+            {
+                player.Stop();
+                musicPlaying = false;
+                time.Stop();
+                progressBar.Value = 0;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //no action (its just a label)
+        }
+
+        private void musicNameBox1_TextChanged(object sender, EventArgs e)
+        {
+            //do nothing (i.e. this box just holds the track name)
+        }
+
+        private void MusicTimer()
+        {
+            // Set the interval to 1000 ms for the timer.
+            time.Interval = 1000;
+            // Connect the Tick event of the timer to its event handler.
+            time.Tick += new EventHandler(IncreaseProgressBar);
+            // Start the timer.
+            time.Start();
+        }
+
+        private void IncreaseProgressBar(object sender, EventArgs e)
+        {
+            // Increment the value of the ProgressBar a value of one each time.
+            progressBar.Increment(1);
+
+            // Determine if we have completed by comparing the value of the Value property to the Maximum value.
+            if (progressBar.Value == progressBar.Maximum)
+                // Stop the timer.
+                time.Stop();
         }
     }
 }
